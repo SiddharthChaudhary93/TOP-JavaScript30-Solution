509e7d169f82b757b7d90623ac28067470393ac0
Siddharth Chaudhary <siddharth.93.chaudhary@gmail.com>
2021-04-13 12:57:49 +0530

    finished the function in js need to do it once again after the lunch to further strnghten the concepts learnt

--- drumKit/javascript.js
+++ drumKit/javascript.js
@@ -1,8 +1,31 @@
-const key = document.querySelectorAll('.key');
 
-function display(e){
-    console.log(e);
+
+const displayDetails = (event) =>{
+    console.log(event.keyCode);
+}
+
+const audioCheck = (event) => {
+    const audio = document.querySelector(`audio[data-key='${event.keyCode}']`);
+    const key = document.querySelector(`.key[data-key='${event.keyCode}']`)
+    // console.log(audio);
+    if(!audio) return;  //stop from running all together
+    audio.currentTime=0 // to set the audio from 0 everytime
+    audio.play();
+    // console.log(key);
+    key.classList.add('playing')  //add the transform class
 }
 
 
-key.addEventListener('click',display);
+function removeGlow(event){
+    if(event.propertyName != 'transform') return;
+    console.log(event.propertyName);
+    this.classList.remove('playing');
+}
+
+const key = document.querySelectorAll('.key');
+
+key.forEach( key => {
+    key.addEventListener('transitionend',removeGlow);
+})
+console.log(key);
+window.addEventListener('keydown',audioCheck);
