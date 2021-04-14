c81f4b404ae04cd87d4d82ea3f9c1b29517b088f
Siddharth Chaudhary <siddharth.93.chaudhary@gmail.com>
2021-04-14 10:32:06 +0530

    practiced the clock face again and remember to use top w.r.t. the height of the page and learn how to use left property properly

--- jsClock/javascript.js
+++ jsClock/javascript.js
@@ -2,23 +2,22 @@ const secondHand = document.querySelector('.second-hand');
 const minuteHand = document.querySelector('.minute-hand');
 const hourHand = document.querySelector('.hour-hand');
 
-
-function setDate(){
+function getTime(){
     const date = new Date();
     const seconds = date.getSeconds();
-    const minute = date.getMinutes();
-    let hour = date.getHours();
-    console.log(hour,minute,seconds);
-    const secondsDegree = ( (seconds / 60) * 360 ) + 90;
-    const minuteDegree = ( (minute / 60) * 360 ) + 90;
+    const minutes = date.getMinutes();
+    var hour = date.getHours();
     if(hour>12){
-        hour=hour-12;
+        hour = hour -12;
     }
-    const hourDegree = ( (hour / 12) * 360 ) + 90;
-    secondHand.style.transform = `rotate(${secondsDegree}deg)`;
+    console.log(hour,minutes,seconds);
+
+    const secondDegree = ( (seconds / 60) * 360) + 90;
+    const minuteDegree = ( (minutes / 60) *360) + 90;
+    const hourDegree = ( (hour / 12) * 360) + 90;
+    secondHand.style.transform = `rotate(${secondDegree}deg)`;
     minuteHand.style.transform = `rotate(${minuteDegree}deg)`;
-    hourHand.style.transform = `rotate(${hourHand}deg)`;
-    
+    hourHand.style.transform = `rotate(${hourDegree}deg)`;
 }
 
-setInterval(setDate,1000);
+setInterval(getTime,1000);
